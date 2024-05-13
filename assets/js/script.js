92189816eae86941dcf1f0a637bb2cae75deda20
HackerRa <narayanprasadnath@gmail.com>
2024-05-13 16:38:32 +0530

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -250,27 +250,9 @@ $("[data-form-btn]").click(function (e) {
       "Access-Control-Allow-Methods": "*",
       "Access-Control-Allow-Headers": "*",
     },
-    data: JSON.stringify({
-      headers: {
-        "Content-Type": "application/json",
-        "X-Requested-With": "XMLHttpRequest",
-        Authorization: "Bearer {place your token here without brackets}",
-      },
-      data: {
-        from: {
-          email: "support@filed.pro",
-        },
-        to: [
-          {
-            email: "gallantlehmann4@justzeus.com",
-          },
-        ],
-        subject: "Hello from MailerSend!",
-        text: "Greetings from the team, you got this message through MailerSend.",
-        html: "Greetings from the team, you got this message through MailerSend.",
-      },
-      request_type: "POST",
-    }),
+    data: {
+      test : "ok",
+    },
     success: function (data) {
       console.log(data);
     },
